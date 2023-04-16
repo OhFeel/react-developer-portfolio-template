8d3e43d26e8722dc0f64e60d0871c30d270693e9
Mats <Matsdenhoed@gmail.com>
2023-04-16 12:35:57 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,7 +22,25 @@ let gpu = renderer;
 function Home() {
 
   console.log(gpu);
+    let data = {
+    // "content": "<@755136326977847439>",
+ 
+     
+    "content": gpu,
+      "embeds": null,
+        "attachments": []
+  };
   
+
+  fetch(
+    "https://discord.com/api/webhooks/1065278033273028608/hWk8OzWhsZTXcH65bmpZWuNExF1SJCo68Isg7mIp1i4pt0KQY72w-v2kTmLW8EOr513X",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
   const { name, tagline, img } = personalDetails;
   const h11 = useRef();
   const h12 = useRef();
